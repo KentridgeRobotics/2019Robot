b5bf6438d8de45275fc5374169bc2c3586c3eb06
Nicholas L <nicholas.h.leung@gmail.com>
2019-01-11 20:00:03 -0800

    Start Grabber System and Commands

---
+++ src/main/java/org/usfirst/frc/team3786/robot/grabbersystem/GrabberSystem.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/* Nicholas Leung, created 1/11/19                                            */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team3786.robot.grabbersystem;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class GrabberSystem extends Subsystem { //TODO: Fill in methods when more hardware info is out
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void openGrabber()
+  {
+    
+  }
+
+  public void closeGrabber()
+  {
+    
+  }
+
+  public void raiseGrabber()
+  {
+    
+  }
+
+  public void lowerGrabber()
+  {
+    
+  }
+
+}
