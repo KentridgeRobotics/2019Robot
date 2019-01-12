5f25904912e0ea9c8bbe835a3b7e982a1cc3e6e0
PseudoResonance <kaio11602@gmail.com>
2019-01-12 00:56:44 -0800

    Fix

--- src/main/java/org/usfirst/frc/team3786/robot/subsystems/vision/Cameras.java
+++ src/main/java/org/usfirst/frc/team3786/robot/subsystems/vision/Cameras.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3786.robot.subsystems.vision;
 import org.usfirst.frc.team3786.robot.utils.pixy2.links.SPILink;
 
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode.PixelFormat;
 import edu.wpi.first.cameraserver.CameraServer;
 
 public class Cameras {
@@ -22,6 +23,7 @@ public class Cameras {
     public static void initDrive() {
         drive = CameraServer.getInstance().startAutomaticCapture();
         if (drive != null) {
+			drive.setVideoMode(PixelFormat.kBGR, 320, 240, 30);
 			drive.setResolution(320, 240);
             drive.setFPS(30);
             drive.setWhiteBalanceManual(5000);
