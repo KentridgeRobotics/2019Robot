6f5e790143cf9e7a4aa0f9334a25a1d256126349
PseudoResonance <kaio11602@gmail.com>
2019-01-19 20:06:54 -0800

    Camera Updates

--- src/main/java/org/usfirst/frc/team3786/robot/subsystems/vision/PixyCamera.java
+++ src/main/java/org/usfirst/frc/team3786/robot/subsystems/vision/PixyCamera.java
@@ -9,29 +9,29 @@ import io.github.pseudoresonance.pixy2api.links.Link;
 
 public class PixyCamera {
 
-    private final Pixy2 pixy;
+	private final Pixy2 pixy;
 
-    public PixyCamera(Link link) {
-        pixy = Pixy2.createInstance(link);
-        pixy.init();
-    }
+	public PixyCamera(Link link) {
+		pixy = Pixy2.createInstance(link);
+		pixy.init();
+	}
 
-    public PixyCamera(Link link, int arg) {
-        pixy = Pixy2.createInstance(link);
-        pixy.init(arg);
-    }
+	public PixyCamera(Link link, int arg) {
+		pixy = Pixy2.createInstance(link);
+		pixy.init(arg);
+	}
 
-    public void run() {
-        pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
-        ArrayList<Block> blocks = pixy.getCCC().getBlocks();
-        //System.out.println("Blocks Found: " + blocks.size());
-        for (Block b : blocks) {
-            System.out.println(b.toString());
-        }
-    }
+	public void run() {
+		pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+		ArrayList<Block> blocks = pixy.getCCC().getBlocks();
+		// System.out.println("Blocks Found: " + blocks.size());
+		for (Block b : blocks) {
+			// System.out.println(b.toString());
+		}
+	}
 
-    public Pixy2 getPixy() {
-        return pixy;
-    }
+	public Pixy2 getPixy() {
+		return pixy;
+	}
 
 }
