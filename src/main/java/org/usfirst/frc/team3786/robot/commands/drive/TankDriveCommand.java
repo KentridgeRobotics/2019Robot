332d237497eae3751d026a4ca40a5d131e7bc8b0
Matthew <appmaker4fun@outlook.com>
2019-01-26 14:15:02 -0800

    Updated TankDriveCommand.

--- src/main/java/org/usfirst/frc/team3786/robot/commands/drive/TankDriveCommand.java
+++ src/main/java/org/usfirst/frc/team3786/robot/commands/drive/TankDriveCommand.java
@@ -33,8 +33,8 @@ public class TankDriveCommand extends Command {
 		double leftStickY = OI.getPrimaryController().getLeftStickY();
 		double leftStickX = OI.getPrimaryController().getLeftStickX();
 		double leftTrigger = OI.getPrimaryController().getLeftTrigger();
-		TankDriveSubsystem.getInstance().arcadeDrive(leftStickY * leftTrigger / 2.0, leftStickX * leftTrigger / 2.0);
-	}
+		TankDriveSubsystem.getInstance().arcadeDrive(leftStickY * leftTrigger - 1 / 2.0, leftStickX * leftTrigger - 1 / 2.0);
+	}-
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
