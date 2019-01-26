20ceb012cdcae98f9fbba4db5f59f717559ba38f
Matthew <appmaker4fun@outlook.com>
2019-01-26 14:37:53 -0800

    Removed minus sign after the curly brackets.

--- src/main/java/org/usfirst/frc/team3786/robot/commands/drive/TankDriveCommand.java
+++ src/main/java/org/usfirst/frc/team3786/robot/commands/drive/TankDriveCommand.java
@@ -34,7 +34,7 @@ public class TankDriveCommand extends Command {
 		double leftStickX = OI.getPrimaryController().getLeftStickX();
 		double leftTrigger = OI.getPrimaryController().getLeftTrigger();
 		TankDriveSubsystem.getInstance().arcadeDrive(leftStickY * (leftTrigger - 1) / 2.0, leftStickX * (leftTrigger - 1) / 2.0);
-	}-
+	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
