cd8bfe3cde90e769dd5491848488e4d12694f92e
Nicholas L <nicholas.h.leung@gmail.com>
2019-01-26 13:30:59 -0800

    Added constants class for numbers, added telemetry for Grabber current

---
+++ src/main/java/org/usfirst/frc/team3786/robot/NumConstants.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team3786.robot;
+
+public class NumConstants
+{
+    public static final int GRABBER_AMP_LIMIT = 20; //Constant for maximum current to Grabber motor
+}
--- src/main/java/org/usfirst/frc/team3786/robot/commands/grabber/GrabberCloseCommand.java
+++ src/main/java/org/usfirst/frc/team3786/robot/commands/grabber/GrabberCloseCommand.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3786.robot.commands.grabber;
 
+import org.usfirst.frc.team3786.robot.Dashboard;
+import org.usfirst.frc.team3786.robot.NumConstants;
 import org.usfirst.frc.team3786.robot.subsystems.GrabberSubsystem;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -21,6 +23,15 @@ public class GrabberCloseCommand extends Command {
 	@Override
 	protected void execute() {
 		GrabberSubsystem.getInstance().setGrabberSpeed(-0.5);
+		Dashboard.getInstance().putNumber(false, "Grabber Current (Amps)", GrabberSubsystem.getInstance().getGrabberCurrent());
+		if(GrabberSubsystem.getInstance().getGrabberCurrent() > NumConstants.GRABBER_AMP_LIMIT)
+		{
+			Dashboard.getInstance().putBoolean(true, "Grabber Overcurrent", true);
+		}
+		else
+		{
+			Dashboard.getInstance().putBoolean(true, "Grabber Overcurrent", false);
+		}
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
