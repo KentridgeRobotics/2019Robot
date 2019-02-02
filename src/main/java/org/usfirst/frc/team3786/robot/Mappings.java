80e71b240de43a36dfaa16c0e94cc941df719990
Matthew <appmaker4fun@outlook.com>
2019-02-02 09:51:20 -0800

    Updated Bindings in Mappings.java

--- src/main/java/org/usfirst/frc/team3786/robot/Mappings.java
+++ src/main/java/org/usfirst/frc/team3786/robot/Mappings.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team3786.robot;
 
 import org.usfirst.frc.team3786.robot.commands.grabber.GrabberCloseCommand;
+import org.usfirst.frc.team3786.robot.commands.grabber.GrabberInCommand;
 import org.usfirst.frc.team3786.robot.commands.grabber.GrabberOpenCommand;
+import org.usfirst.frc.team3786.robot.commands.grabber.GrabberOutCommand;
 import org.usfirst.frc.team3786.robot.commands.grabber.GrabberStopCommand;
 import org.usfirst.frc.team3786.robot.subsystems.ElevatorSubsystem.Levels;
 import org.usfirst.frc.team3786.robot.subsystems.ElevatorSubsystem.VerticalDirection;
@@ -34,27 +36,36 @@ public class Mappings {
 	public final static int elevatorMotor = 12; // Lift, SPARK Max
 
 	public static void setupDefaultMappings() {
+		
 		XboxController primary = OI.getPrimaryController();
 		primary.buttonA.whenPressed(new BrakeOnCommand());
 		primary.buttonA.whenReleased(new BrakeOffCommand());
-		primary.buttonX.whenPressed(new ElevatorDownCommand());
-		primary.buttonX.whenReleased(new ElevatorStopCommand());
 		primary.buttonB.whenPressed(new BoostOnCommand());
 		primary.buttonB.whenReleased(new BoostOffCommand());
+		primary.buttonX.whenPressed(new ElevatorDownCommand());
+		primary.buttonX.whenReleased(new ElevatorStopCommand());
 		primary.buttonY.whenPressed(new ElevatorUpCommand());
 		primary.buttonY.whenReleased(new ElevatorStopCommand());
 		primary.buttonBumperLeft.whenPressed(new ElevatorChangeCommand(VerticalDirection.UP));
 		primary.buttonBumperLeft.whenReleased(new ElevatorChangeCommand(VerticalDirection.STOP));
 		primary.buttonBumperRight.whenPressed(new ElevatorChangeCommand(VerticalDirection.DOWN));
 		primary.buttonBumperRight.whenReleased(new ElevatorChangeCommand(VerticalDirection.STOP));
+		
 		XboxController secondary = OI.getSecondaryController();
 		GrabberStopCommand grabberStopCommand = new GrabberStopCommand();
-		secondary.buttonA.whenPressed(new GrabberOpenCommand());
+		secondary.buttonA.whenPressed(new GrabberInCommand());
 		secondary.buttonA.whenReleased(grabberStopCommand);
-		secondary.buttonB.whenPressed(new GrabberCloseCommand());
+		secondary.buttonB.whenPressed(new GrabberOutCommand());
 		secondary.buttonB.whenReleased(grabberStopCommand);
-		secondary.buttonBumperRight.whenPressed(new ElevatorSendCommand(Levels.ZERO));
 		secondary.buttonY.whenPressed(new ClimbWhileLevelCommand());
+		secondary.buttonBumperLeft.whenPressed(new ElevatorChangeCommand(VerticalDirection.UP));
+		secondary.buttonBumperLeft.whenReleased(new ElevatorChangeCommand(VerticalDirection.STOP));
+		secondary.buttonBumperRight.whenPressed(new ElevatorChangeCommand(VerticalDirection.DOWN));
+		secondary.buttonBumperRight.whenReleased(new ElevatorChangeCommand(VerticalDirection.STOP));
+		secondary.buttonTriggerLeft.whenPressed(new GrabberOpenCommand());
+		secondary.buttonTriggerLeft.whenReleased(grabberStopCommand);
+		secondary.buttonTriggerRight.whenPressed(new GrabberOpenCommand());
+		secondary.buttonTriggerRight.whenReleased(grabberStopCommand);
 	}
 
 	public static void setupTestMappings() {
