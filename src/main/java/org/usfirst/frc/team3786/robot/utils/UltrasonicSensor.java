c3322bb3485de2ce3a80e007196a2d9be18b5c5a
Nicholas L <nicholas.h.leung@gmail.com>
2019-02-02 10:58:36 -0800

    Updated UltrasonicSensor.java

--- src/main/java/org/usfirst/frc/team3786/robot/utils/UltrasonicSensor.java
+++ src/main/java/org/usfirst/frc/team3786/robot/utils/UltrasonicSensor.java
@@ -1,9 +1,17 @@
 package org.usfirst.frc.team3786.robot.utils;
 
+import org.usfirst.frc.team3786.robot.Mappings;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
 public class UltrasonicSensor implements DistanceSensorInterface {
+
+    private AnalogInput distSensor = new AnalogInput(Mappings.UltrasonicSensor);
+
     @Override
     public double getDistanceCm() {
-        //put voltage calculations here later.
-        return 0;
+        double calcDist;
+        calcDist = ((distSensor.getAverageVoltage() / 1000) / 300) * 10; //convert to mV, then divide to find mm, finally convert to cm.
+        return calcDist;
     }
 }
