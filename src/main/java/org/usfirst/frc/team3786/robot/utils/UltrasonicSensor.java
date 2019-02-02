c67593443ebca00a287fcb018ee31c2a86cd2f8b
Nicholas L <nicholas.h.leung@gmail.com>
2019-02-02 13:52:04 -0800

    Updated UltrasonicSensor.java

--- src/main/java/org/usfirst/frc/team3786/robot/utils/UltrasonicSensor.java
+++ src/main/java/org/usfirst/frc/team3786/robot/utils/UltrasonicSensor.java
@@ -1,8 +1,10 @@
 package org.usfirst.frc.team3786.robot.utils;
 
+import org.usfirst.frc.team3786.robot.Dashboard;
 import org.usfirst.frc.team3786.robot.Mappings;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class UltrasonicSensor implements DistanceSensorInterface {
 
@@ -19,6 +21,7 @@ public class UltrasonicSensor implements DistanceSensorInterface {
     @Override
     public double getDistanceCm() {
         double calcDist;
+        SmartDashboard.putNumber("Ultrasonic Avg Bits", distSensor.getAverageBits());
         calcDist = (300.0 * (distSensor.getAverageVoltage() / 0.293)) / 10.0; 
         return calcDist;
     }
