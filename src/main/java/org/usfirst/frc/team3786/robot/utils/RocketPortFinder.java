41daa3aedd79fb13977515391f28a8b4ed217414
Nicholas L <nicholas.h.leung@gmail.com>
2019-02-17 15:51:57 -0800

    Updated RocketPortFinder, remove unused import statement

--- src/main/java/org/usfirst/frc/team3786/robot/utils/RocketPortFinder.java
+++ src/main/java/org/usfirst/frc/team3786/robot/utils/RocketPortFinder.java
@@ -1,7 +1,34 @@
 package org.usfirst.frc.team3786.robot.utils;
 
+import org.usfirst.frc.team3786.robot.subsystems.vision.Cameras;
+import org.usfirst.frc.team3786.robot.subsystems.vision.PixyCamera;
+
+import io.github.pseudoresonance.pixy2api.Pixy2Line.Vector;
+
 public class RocketPortFinder {
-    public static double calcTurn() { //add pixy cam code later
-        return 90.0;
+    
+    public static double calcTurn () {
+        PixyCamera pixy = Cameras.getPixyCamera2();
+        if(pixy == null) {
+            return 0.0;
+        }
+        else {
+            Vector[] vectors = pixy.getPixy().getLine().getVectors();
+            if(vectors != null) {
+                if(vectors.length > 0) {
+                    Vector vector = vectors[0];
+                    double leg1 = vector.getX0() - vector.getX1();
+                    double leg2 = vector.getY1() - vector.getY0();
+                    double theta = Math.toDegrees(Math.atan2(leg2, leg1));
+                    return theta;
+                }
+                else {
+                    return 0.0;
+                }
+            }
+            else {
+                return 0.0;
+            }
+        }
     }
 }
