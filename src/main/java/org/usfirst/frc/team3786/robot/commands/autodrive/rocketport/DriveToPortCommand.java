9ad1ef3084849e89c28a168c14fc9dcf378718f9
PseudoResonance <kaio11602@gmail.com>
2019-03-17 15:29:07 -0700

    Positive Drive Direction and Ultrasonic in Auto

--- src/main/java/org/usfirst/frc/team3786/robot/commands/autodrive/rocketport/DriveToPortCommand.java
+++ src/main/java/org/usfirst/frc/team3786/robot/commands/autodrive/rocketport/DriveToPortCommand.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team3786.robot.commands.autodrive.rocketport;
 
 import org.usfirst.frc.team3786.robot.subsystems.NeoDriveSubsystem;
 import org.usfirst.frc.team3786.robot.subsystems.vision.TargetLine;
+import org.usfirst.frc.team3786.robot.utils.UltrasonicSensor;
+import org.usfirst.frc.team3786.robot.utils.UltrasonicSensor.Side;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,8 +11,8 @@ public class DriveToPortCommand extends Command {
 
 	private static final double epsilon = 3.0;
 	private static final double turningEpsilon = 10.0;
-	private static final double positionThreshold = 10.0;
-	private static final int lostLineThreshold = 4;
+	private static final double positionEpsilon = 10.0;
+	private static final int lostLineLimit = 4;
 
 	private double position = 0.0;
 	private double correction = 0.0;
@@ -37,29 +39,41 @@ public class DriveToPortCommand extends Command {
 		} else {
 			lostLineDuration++;
 		}
-		if (hasRunOnce && lostLineDuration < lostLineThreshold) {
-			if ((correction >= turningEpsilon && position < -positionThreshold)
-					|| (correction <= -turningEpsilon && position > positionThreshold)) {
+		if (hasRunOnce && lostLineDuration < lostLineLimit) {
+			if ((correction >= turningEpsilon && position < -positionEpsilon)
+					|| (correction <= -turningEpsilon && position > positionEpsilon)) {
 				NeoDriveSubsystem.getInstance().arcadeDrive(0.1, 0);
-			} else if (correction < -turningEpsilon && position < -positionThreshold) {
+				System.out.println("DriveToPort: Forward - Position Correction");
+			} else if (correction < -turningEpsilon && position < -positionEpsilon) {
 				NeoDriveSubsystem.getInstance().arcadeDrive(0, 0.05);
-			} else if (correction > turningEpsilon && position > positionThreshold) {
+				System.out.println("DriveToPort: Right Turn");
+			} else if (correction > turningEpsilon && position > positionEpsilon) {
 				NeoDriveSubsystem.getInstance().arcadeDrive(0, -0.05);
-			} else if (Math.abs(position) <= positionThreshold) {
+				System.out.println("DriveToPort: Left Turn");
+			} else if (Math.abs(position) <= positionEpsilon) {
 				if (correction > epsilon) {
 					NeoDriveSubsystem.getInstance().arcadeDrive(0, 0.05);
+					System.out.println("DriveToPort: Right Turn");
 				} else if (correction < -epsilon) {
 					NeoDriveSubsystem.getInstance().arcadeDrive(0, -0.05);
+					System.out.println("DriveToPort: Left Turn");
 				} else if (Math.abs(correction) < epsilon) {
-					System.out.println("ALIGNED TO LINE");
-					isDone = true;
+					System.out.println("DriveToPort: Aligned to Line");
+					if (UltrasonicSensor.getInstance(Side.LEFT).getDistanceCM() > 50 && UltrasonicSensor.getInstance(Side.RIGHT).getDistanceCM() > 50) {
+						NeoDriveSubsystem.getInstance().arcadeDrive(0.1, 0);
+					} else {
+						NeoDriveSubsystem.getInstance().arcadeDrive(0, 0);
+						System.out.println("DriveToPort: Reached Wall");
+						isDone = true;
+					}
 				}
 			}
-		} else if (hasRunOnce && lostLineDuration >= lostLineThreshold) {
+		} else if (hasRunOnce && lostLineDuration >= lostLineLimit) {
 			NeoDriveSubsystem.getInstance().arcadeDrive(0, 0);
-			System.out.println("FAILED TO FIND LINE");
+			System.out.println("DriveToPort: Line Lost");
 			isDone = true;
 		} else if (!hasRunOnce) {
+			System.out.println("DriveToPort: Failed to Find Line");
 			NeoDriveSubsystem.getInstance().arcadeDrive(0, 0);
 		}
 	}
